d40e47d50d8446810b15c61f1e03796bf8532298
ParthKal1712 <139514797+ParthKal1712@users.noreply.github.com>
2023-11-16 15:33:15 -0700

    Created models

---
+++ models/hospital-management/patient.model.js
@@ -0,0 +1,45 @@
+import mongoose from "mongoose";
+
+const patientSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: true,
+        },
+        diagnosedWith: {
+            type: String,
+            required: true,
+        },
+        doctor: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Doctor",
+            required: true,
+        },
+        address: {
+            type: String,
+            required: true,
+        },
+        age: {
+            type: Number,
+            required: true,
+        },
+        bloodGroup: {
+            type: String,
+            enum: ["A+", "A-", "B+", "B-", "AB+", "AB-", "O+", "O-"],
+            required: true,
+        },
+        gender: {
+            type: String,
+            enum: ["M", "F", "O"],
+            required: true,
+        },
+        admittedIn: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Hospital",
+            required: true,
+        },
+    },
+    { timestamps: true }
+);
+
+export const Patient = mongoose.model("Patient", patientSchema);
