7b6f787c06e8c62508b41726c82694bb4b9d3f70
ParthKal1712 <139514797+ParthKal1712@users.noreply.github.com>
2023-11-17 15:11:51 -0700

    Added more models, jwt and bcrypt

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,4 +1,6 @@
 import mongoose from "mongoose";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
 
 const userSchema = new mongoose.Schema(
   {
@@ -7,27 +9,85 @@ const userSchema = new mongoose.Schema(
       required: true,
       unique: true,
       lowercase: true,
+      trim: true,
+      index: true,
     },
     password: {
       type: String,
       required: true,
     },
-    userId: {
+    fullName: {
+      type: String,
+      required: true,
+      index: true,
+      trim: true,
+    },
+    email: {
       type: String,
       required: true,
     },
-    name: {
-      first: {
-        type: String,
-        required: true,
-      },
-      last: {
-        type: String,
-        required: true,
-      },
+    avatar: {
+      type: String, //image url
+      required: true,
+    },
+    coverImage: {
+      type: String, //image url
+    },
+    watchHistory: {
+      type: [mongoose.Schema.Types.ObjectId],
+      ref: "Video",
+    },
+    refreshToken: {
+      type: String,
     },
   },
   { timestamps: true }
 );
+
+//ADDING isPasswordCorrect METHOD TO THE SCHEMA
+//CANNOT USE ARROW FUNCTIONS HERE BECAUSE WE NEED TO REFER TO THE INSIDE OBJECT AND ARROW FUNCTIONS DONT ALLOW CONTEXT
+userSchema.methods.isPasswordCorrect = async function (password) {
+  //THIS FUNCTION TAKES IN [PLAIN TEXT PASSWORD, ENCRYPTED PASSWORD], COMPARES THEM AND RETURNS IF THEY MATCH OR NOT
+  return await bcrypt.compare(password, this.password);
+};
+
+userSchema.methods.generateAccessToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+      email: this.email,
+      username: this.username,
+      fullName: this.fullName,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
+    }
+  );
+};
+
+userSchema.methods.generateRefreshToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+      expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
+    }
+  );
+};
+
+//THIS IS A MIDDLEWARE THAT WILL BE CALLED EVERY TIME BEFORE THE DOCUMENT IS SAVED
+userSchema.pre("save", function (next) {
+  //CHECK IF THE PASSWORD IS MODIFIED
+  if (this.isModified("password")) {
+    //IF IT IS MODIFIED, ENCRYPT THE NEW PASSWORD
+    this.password = bcrypt.hash(this.password, 10);
+  }
+
+  //CALL THE NEXT MIDDLEWARE OR MOVE ALONG..
+  next();
+});
 
 export const User = mongoose.model("User", userSchema);
